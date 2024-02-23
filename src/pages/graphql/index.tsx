0ecdf713c8e61f8184bb0a57c26d230a3a2c5bef
Frank <frank.pfeift@protonmail.com>
2024-02-23 09:53:29 +0100

    chore: added query param to graphql playground

--- src/pages/graphql/index.tsx
+++ src/pages/graphql/index.tsx
@@ -1,5 +1,8 @@
 import { useRouter } from "next/router";
 
 export default () => {
-    return <iframe src={`https://ph-switchboard-nginx-prod-c84ebf8c6e3b.herokuapp.com/explorer`} height="100%" width="100%" className="min-h-[calc(100vh-63px)]" />;
+    const router = useRouter();
+    const query = router.query.query;
+
+    return <iframe src={`https://ph-switchboard-nginx-prod-c84ebf8c6e3b.herokuapp.com/explorer${query ? "?query=" + query : ""}`} height="100%" width="100%" className="min-h-[calc(100vh-63px)]" />;
 };
